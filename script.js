44028da4af0db634a1fa4b47fd199f91c1fbed19
Executer <lokomas@inbox.ru>
2023-07-15 16:24:11 +0700

    Adding game score display on the page

--- script.js
+++ script.js
@@ -2,24 +2,62 @@ const playerDecision = document.querySelectorAll('.game');
 const valuesGame = ['choice_knight', 'choice_dragon', 'choice_wizard'];
 const rules = {
     'choice_knight': {
-        'choice_dragon': 'The knight defeats the dragon. You win!',
-        'choice_wizard': 'The knight was bewitched by the wizard before the blow. You lost!',
-        'choice_knight': 'The knights attack each other, but to no avail. It\'s a tie!'
+        'choice_dragon': {
+            message: 'The knight defeats the dragon. You win!',
+            playerScore: 1,
+            computerScore: 0
+        },
+        'choice_wizard': {
+            message: 'The knight was bewitched by the wizard before the blow. You lost!',
+            playerScore: 0,
+            computerScore: 1
+        },
+        'choice_knight': {
+            message: 'The knights attack each other, but to no avail. It\'s a tie!',
+            playerScore: 0,
+            computerScore: 0
+        }
     },
     'choice_dragon': {
-        'choice_knight': 'The dragon falls into the knight\'s trap and is killed by his sword. You lost!',
-        'choice_wizard': 'The dragon maneuvers away from the wizard\'s spells and burns him with its breath. You win!',
-        'choice_dragon': 'The dragons in the sky try to hurt each other, but to no avail. It\'s a tie!'
+        'choice_knight': {
+            message: 'The dragon falls into the knight\'s trap and is killed by his sword. You lost!',
+            playerScore: 0,
+            computerScore: 1
+        },
+        'choice_wizard': {
+            message: 'The dragon maneuvers away from the wizard\'s spells and burns him with its breath. You win!',
+            playerScore: 1,
+            computerScore: 0,
+        },
+        'choice_dragon': {
+            message: 'The dragons in the sky try to hurt each other, but to no avail. It\'s a tie!',
+            playerScore: 0,
+            computerScore: 0,
+        }
     },
     'choice_wizard': {
-        'choice_knight': 'The wizard manages to send a fireball towards the knight and destroys it. You win!',
-        'choice_dragon': 'The wizard fails to cast a spell on the dragon and is killed by its breath. You\'ve lost',
-        'choice_wizard': 'Wizards anticipate each other\'s spells and manage to defend themselves. It\'s a tie!'
+        'choice_knight': {
+            message: 'The wizard manages to send a fireball towards the knight and destroys it. You win!',
+            playerScore: 1,
+            computerScore: 0,
+        },
+        'choice_dragon': {
+            message: 'The wizard fails to cast a spell on the dragon and is killed by its breath. You\'ve lost',
+            playerScore: 0,
+            computerScore: 1
+        },
+        'choice_wizard': {
+            message: 'Wizards anticipate each other\'s spells and manage to defend themselves. It\'s a tie!',
+            playerScore: 0,
+            computerScore: 0
+        }
     }
 };
 let playerChoice = null;
 let computerChoice = null;
 let result = null;
+let playerScore = null;
+let computerScore = null;
 
 playerDecision.forEach(button => {
     button.addEventListener('click', () => processGame(button));
@@ -30,6 +68,8 @@ function processGame(choice) {
     choiceComputer();
     displayChoices()
     compareChoices();
+    displayReasonScore();
+    displayScore();
 }
 
 function choicePlayer(button) {
@@ -57,6 +97,21 @@ function displayChoices() {
 }
 
 function compareChoices() {
-    result = rules[playerChoice][computerChoice];
-    return result;
+    resultCompare = rules[playerChoice][computerChoice].message;
+    return resultCompare;
+}
+
+function displayReasonScore() {
+    const displayReasonScore = document.querySelector('.board__reason');
+    displayReasonScore.innerHTML = resultCompare;
+}
+
+function displayScore() {
+    const displayScoreBoard = document.querySelectorAll('.score');
+
+    playerScore += rules[playerChoice][computerChoice].playerScore;
+    displayScoreBoard[0].textContent = playerScore
+
+    computerScore += rules[playerChoice][computerChoice].computerScore;
+    displayScoreBoard[1].textContent = computerScore
 }
